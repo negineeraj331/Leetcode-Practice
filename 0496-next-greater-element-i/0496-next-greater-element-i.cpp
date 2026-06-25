f85765b043a65a33dba0da5ec39dab4a7bc6abf0
Neeraj Negi <151132402+negineeraj331@users.noreply.github.com>
2026-06-25 12:07:40 +0530

    Time: 0 ms (100%), Space: 12.9 MB (38.56%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,28 +1,63 @@
-// monotonic stack
+// // monotonic stack
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//     int n1 = nums1.size();
+//     int n2 = nums2.size();
+
+//     stack<int> st;
+//     vector<int> nge(10001, -1);
+
+//     for(int i=0; i<n2; ++i){
+//         while(!st.empty() && nums2[st.top()]<nums2[i]){
+//             int idx=st.top();
+//             st.pop();
+//             int ele = nums2[idx]; //KEY
+//             nge[ele]=nums2[i];
+//         }
+//         st.push(i);
+//     }
+
+//     vector<int> ans(n1, -1);
+//     for(int i=0;i<n1;++i){
+//         int querykey = nums1[i];
+//         ans[i] = nge[querykey];
+//     }
+//     return ans;
+//     }
+// };
+
+
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-    int n1 = nums1.size();
-    int n2 = nums2.size();
+    vector<int> nextGreaterElement(vector<int>& nums1,
+                                   vector<int>& nums2) {
 
-    stack<int> st;
-    vector<int> nge(10001, -1);
+        stack<int> st;
+        unordered_map<int, int> nextGreater;
 
-    for(int i=0; i<n2; ++i){
-        while(!st.empty() && nums2[st.top()]<nums2[i]){
-            int idx=st.top();
-            st.pop();
-            int ele = nums2[idx]; //KEY
-            nge[ele]=nums2[i];
+        // Process nums2 from right to left
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+
+            if (st.empty())
+                nextGreater[nums2[i]] = -1;
+            else
+                nextGreater[nums2[i]] = st.top();
+
+            st.push(nums2[i]);
         }
-        st.push(i);
-    }
 
-    vector<int> ans(n1, -1);
-    for(int i=0;i<n1;++i){
-        int querykey = nums1[i];
-        ans[i] = nge[querykey];
-    }
-    return ans;
+        vector<int> ans;
+
+        // Answer queries for nums1
+        for (int num : nums1) {
+            ans.push_back(nextGreater[num]);
+        }
+
+        return ans;
     }
 };
